200a6f2f1dafaa006c4aaa1685e7e0b7d522a6f3
Cristian Recabarren <cristian.recabarren.madrid@gmail.com>
2022-08-25 15:19:21 -0400

    Update readLine.h

--- readLine.h
+++ readLine.h
@@ -2,6 +2,12 @@
 #ifndef __READLINE_H__
 #define __READLINE_H__
 
+/**
+ * Reads a line from stdin and it finish when user press Enter key
+ * (or read end of carriage char). Also returns the amount of characters
+ * read into chars variable.
+ * returns char* a pointer to the read string.
+ */
 char* readLine(int *chars);
 
 #endif
