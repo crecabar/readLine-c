49d98a8ec0efb0665367b3dbdaed727c0841471b
Cristian Recabarren <cristian.recabarren.madrid@gmail.com>
2022-08-25 15:18:58 -0400

    Update readLine.c

--- readLine.c
+++ readLine.c
@@ -4,6 +4,12 @@
 
 //#include <readLine.h>
 
+/**
+ * Reads a line from stdin and it finish when user press Enter key
+ * (or read end of carriage char). Also returns the amount of characters
+ * read into chars variable.
+ * returns char* a pointer to the read string.
+ */
 char * readLine(int *chars)
 {
     int count = 0;
